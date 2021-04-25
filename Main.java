4dc99ae1ed8a876aec89501b720644e60c228648
SulaimanWaleed <61086934+SulaimanWaleed@users.noreply.github.com>
2021-04-25 21:55:33 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,19 @@ public class Main {
 
 	public static void main(String[] args) {
 	int[] numbers = {1,2,3,4,5,6};
-		for(int i = 0; i<numbers.length; i++){
-			System.out.println(numbers[i]);
+		
+			printArray(numbers);
 	}
 
-	public void printArray(int[] array){
+	public static void printArray(int[] array){
 		if(array.length != 0)
 		{
 			System.out.println(array[0]);
-			printArray(array[1]);
+			int[] newArray = new int[array.length -1];
+			for(int i = 1; i<array.length; i++) {
+				newArray[i-1] = array[i];
+			}
+			printArray(newArray);
 		}
 	}
 }
