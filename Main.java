47ff6b53d23c6c5791b5133433bb541658169843
SulaimanWaleed <61086934+SulaimanWaleed@users.noreply.github.com>
2021-04-25 21:42:21 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		MySquare a = new MySquare(3);
+		MySquare b = new MySquare(2);
 		System.out.println(a.area());
+		System.out.println(b.area());
 	}
 
 }
